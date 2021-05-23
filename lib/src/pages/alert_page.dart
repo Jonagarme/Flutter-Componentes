74712fad4fc1d66df6f7228d68de57e5904a7076
Jonagarme <garciajonathan268@gmail.com>
2021-05-22 23:00:40 -0500

    subiendo todo

---
+++ lib/src/pages/alert_page.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class AlertPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Alert Page'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Mostrar alerta'),
+          onPressed: () => _mostrarAlert(context),
+          style: ElevatedButton.styleFrom(
+            primary: Colors.blue,
+            shape: StadiumBorder(),
+            onPrimary: Colors.white,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add_location),
+        onPressed: () {
+          Navigator.pop(context);
+        },
+      ),
+    );
+  }
+
+  void _mostrarAlert(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          title: Text('Titulo'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text('Este ese el contenido de la caja'),
+              FlutterLogo(
+                size: 100.0,
+              ),
+            ],
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Cancelar'),
+            ),
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: Text('Aceptar'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
